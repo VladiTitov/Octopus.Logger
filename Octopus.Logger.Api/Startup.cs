61d47df1a9142399e1d8c644d28cde7e501f46a4
Vladititov <capt.Hoffmann@gmail.com>
2022-01-21 10:42:28 +0300

    added swagger in project

--- Octopus.Logger.Api/Startup.cs
+++ Octopus.Logger.Api/Startup.cs
@@ -12,13 +12,12 @@ namespace Octopus.Logger.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,13 +26,13 @@ namespace Octopus.Logger.Api
             }
 
             app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
